8686f315f8a99db82f0fa60645102d5fabe855d0
Akinori Wada <akinori.w88a@gmail.com>
2016-11-16 00:52:57 +0000

    11160952

--- db/migrate/20161115004000_add_age_to_messages.rb
+++ db/migrate/20161115004000_add_age_to_messages.rb
@@ -1,5 +1,5 @@
 class AddAgeToMessages < ActiveRecord::Migration
   def change
-    add_column :messages, :age, :integer　# [形式] add_column(テーブル名, カラム名, データ型)
+    add_column :messages, :age, : integer　# [形式] add_column(テーブル名, カラム名, データ型)
   end
 end
